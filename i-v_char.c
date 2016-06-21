c3edccebc3fb3aa2213f2d32db98928ea50828bf
pochiMasahiro <pochi.masahiro@gmail.com>
2016-06-22 01:28:15 +0900

    complete xi

--- i-v_char.c
+++ i-v_char.c
@@ -10,8 +10,18 @@ Masahiro Fukuoka
 #include <string.h>
 #include <ctype.h>
 
-// gannma
-double calc_gannma(double)
+// gamma_g
+double calc_gamma_g(double sigma)
+{
+	return 2*sigma*sqrt(2*log(2.0));
+}
+
+// gamma
+double calc_gamma(double sigma, double gamma_l)
+{
+	double gamma_g = calc_gamma_g(sigma);
+	return pow(pow(gamma_g, 5) + 2.69269*pow(gamma_g, 4)*gamma_l + 2.42843*pow(gamma_g, 3)*pow(gamma_l, 2) + 4.47163*pow(gamma_g, 2)*pow(gamma_l, 3) + 0.07842*gamma_g*pow(gamma_l, 4) + pow(gamma_l, 5), 1/3);
+}
 
 // xi
 double calc_xi(double sigma, double gamma_l)
@@ -19,3 +29,4 @@ double calc_xi(double sigma, double gamma_l)
 	double gamma = calc_gamma(sigma, gamma_l);
 	return 1.36603(gamma_l/gamma) -0.47719*pow((gamma_l/gamma), 2) +0.1116*pow((gamma_l/gamma), 3);
 }
+

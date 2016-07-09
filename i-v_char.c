fb51f4ca8702efbd16696e08719f0e67184324fc
pochiMasahiro <pochi.masahiro@gmail.com>
2016-07-10 01:23:33 +0900

    struct object

--- i-v_char.c
+++ i-v_char.c
@@ -15,13 +15,36 @@ const double k_B = 1.38e-23 / e;
 const double m_ = (9.11e-31)*0.0041;
 const double h_ = 1.05e-34;
 
+typedef struct  {
+	double e;
+	double k_B;
+	double m0;
+	double m;
+	double hbar;
+	double Eta_l;
+	double Eta_r;
+	double E_l0;
+	double E_r0;
+	double J_e;
+	double V_e;
+	double T;
+	double n;
+	double sigma;
+	double gamma_l;
+	double xi;
+	double E_F;
+	double constA;
+	double constB;
+	double area;
+}TBRTDproperties;
+
 
 // electron suppling
-double supply(double E_z, double v, double T, double E_F, double E_c)
+double supply(double E_z, double v, TBRTDproperties prp)
 {
-	double tmp1 = (pow(e, 2)*m_*k_B*T)/(2*pow(M_PI, 2)*pow(h_, 3));
-	double tmp2 = 1.0 + exp((E_F-E_c)/(k_B*T));
-	double tmp3 = 1.0 + exp((E_F-(E_z-E_c+v))/(k_B*T));
+	double tmp1 = (pow(prp.e, 2) * prp.m * prp.k_B * prp.T) / (2*pow(M_PI, 2) * pow(prp.hbar, 3));
+	double tmp2 = 1.0 + exp(prp.E_F / (prp.k_B * prp.T));
+	double tmp3 = 1.0 + exp((prp.E_F - E_z - v) / (prp.k_B * prp.T));
 	return tmp1*log(tmp2/tmp3);
 }
 
@@ -58,64 +81,67 @@ double lorentzian(double x, double x0, double gamma_l)
 }
 
 // transmission probability
-double t_res(double E_z, double V, double gamma_l, double sigma)
+double t_res(double E_z, double V, TBRTDproperties prp)
 {
-	double xi = calc_xi(sigma, gamma_l);
-	return xi*lorentzian(E_z, V, gamma_l) + (1.0 - xi)*gaussian(E_z, V, sigma);
+	return prp.xi * lorentzian(E_z, V, prp.gamma_l) + (1.0 - prp.xi)*gaussian(E_z, V, prp.sigma);
 }
 
-double t_res_integral(double V, double gamma_l, double sigma)
+double t_res_integral(double V, TBRTDproperties prp)
 {
-	double xi = calc_xi(sigma, gamma_l);
-	return (1.0-xi)/M_SQRT2 + (xi/M_PI)*(M_PI_2+atan2(-2.0*V,gamma_l));
+	return (1.0-prp.xi) / M_SQRT2 + (prp.xi / M_PI) * (M_PI_2 + atan2(-2.0 * V, prp.gamma_l));
 }
 
 // thermionic-type of current density
-double j_thermal(double v, double T, double J_e, double V_e, double n)
+double j_thermal(double v, TBRTDproperties prp)
 {
-	double tmp1 = exp(v/(n*k_B*T)) - 1.0;
-	double tmp2 = exp(V_e/(n*k_B*T)) - 1.0;
-	return J_e * tmp1 / tmp2;
+	double tmp1 = exp(v/(prp.n * prp.k_B * prp.T)) - 1.0;
+	double tmp2 = exp(prp.V_e / (prp.n * prp.k_B * prp.T)) - 1.0;
+	return prp.J_e * tmp1 / tmp2;
 }
 
-double E_r(double V)
+double E_r(double V, TBRTDproperties prp)
 {
-	double E_r0 = 97.2e-3;
-	double eta_r = 0.71;
-	return E_r0 - (eta_r*V);
+	return prp.E_r0 - (prp.Eta_r * V);
 }
-long double E_l(double V)
+double E_l(double V, TBRTDproperties prp)
 {
-	double E_l0 = 44.7e-3;
-	double eta_l = 0.353;
-	return E_l0 - (eta_l*V);
+	return prp.E_l0 - (prp.Eta_l * V);
+}
+
+double j_tbrtd(double v, TBRTDproperties prp)
+{
+	double j_rtd_l = supply(E_l(v, prp), v, prp)*t_res(E_l(v, prp), E_r(v, prp), prp)*t_res_integral(E_l(v, prp), prp);
+	double j_rtd_r = supply(E_r(v, prp), v, prp)*t_res(E_r(v, prp), E_l(v, prp), prp)*t_res_integral(E_r(v, prp), prp);
+	double j_th = j_thermal(v, prp);
+	return j_th + prp.constB + (j_rtd_l + j_rtd_r) * prp.constA;
 }
 
 int main(void)
 {
-	double V_e = 0.36;
-	double I_e = 0.60e-3;
-	double T = 100;
-	double n = 3.7;
-	double sigma = 18.0e-3;
-	double gamma_l = 16.0e-3;
-	double E_F = 40e-3;
-	double E_c = 0.0;
-	
+	TBRTDproperties TBRTD;
+	TBRTD.V_e = 0.36;
+	TBRTD.J_e = 0.60e-3;
+	TBRTD.T = 100;
+	TBRTD.n = 3.7;
+	TBRTD.sigma = 18.0e-3;
+	TBRTD.gamma_l = 16.0e-3;
+	TBRTD.xi = calc_xi(TBRTD.sigma, TBRTD.gamma_l);
+	TBRTD.E_F = 40e-3;
+	TBRTD.e = 1.602e-19;
+	TBRTD.k_B = 1.38e-23/e;
+	TBRTD.hbar = 1.05e-34;
+	TBRTD.m0 = 9.11e-31;
+	TBRTD.m = TBRTD.m0 * 0.0041;
+	TBRTD.E_l0 = 44.7e-3;
+	TBRTD.E_r0 = 97.2e-3;
+	TBRTD.Eta_l = 0.353;
+	TBRTD.Eta_r = 0.71;
+	TBRTD.constA = 4.0e-28;
+	TBRTD.constB = 5.0e4;
+
 	double i;
-	double tbrtd = 0.0;
-	double j_rtd = 0.0;
-	double j_rtd_l = 0.0;
-	double j_rtd_r = 0.0;
-	double j_th = 0.0;
-	fprintf(stdout, "V, j_rtd_l, j_rtd_r, j_rtd, j_th\n");       
 	for(i=0; i < 0.4; i+=0.0002) {
-		j_rtd_l = supply(E_l(i), i, T, E_F, E_c)*t_res(E_l(i), E_r(i), gamma_l, sigma)*t_res_integral(E_l(i), gamma_l, sigma);
-		j_rtd_r = supply(E_r(i), i, T, E_F, E_c)*t_res(E_r(i), E_l(i), gamma_l, sigma)*t_res_integral(E_r(i), gamma_l, sigma);
-		j_rtd = (j_rtd_l + j_rtd_r) * 3.0e-28;
-		j_th = j_thermal(i, T, I_e, V_e, n) * 3e6;
-		tbrtd = j_rtd + j_th;
-		fprintf(stdout, "%E %E %E %E %E %E\n", i, j_rtd_l, j_rtd_r, j_rtd, j_th, tbrtd);
+		fprintf(stdout, "%E %E\n", i, j_tbrtd(i, TBRTD));
 	}
 	return EXIT_SUCCESS;
 }

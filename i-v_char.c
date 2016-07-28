09545d77c837515612d6d1299a42589fcebbe421
pochiMasahiro <pochi.masahiro@gmail.com>
2016-07-28 15:34:23 +0900

    modi exapmle i-v_char as use parameter file

--- i-v_char.c
+++ i-v_char.c
@@ -3,40 +3,19 @@ Calculation of I-V characteristic for TBRTD
 2016/06/22
 Masahiro Fukuoka
 */
+#ifndef _MATH
+#define _MATH
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
-#include <string.h>
-#include <ctype.h>
 
-const double e = 1.602e-19;
-const double k_B = 1.38e-23 / e;
-const double m_ = (9.11e-31)*0.0041;
-const double h_ = 1.05e-34;
+#endif
+#ifndef _I_V_CHAR
+#define _I_V_CHAR
 
-typedef struct  {
-	double e;
-	double k_B;
-	double m0;
-	double m;
-	double hbar;
-	double Eta_l;
-	double Eta_r;
-	double E_l0;
-	double E_r0;
-	double J_e;
-	double V_e;
-	double T;
-	double n;
-	double sigma;
-	double gamma_l;
-	double xi;
-	double E_F;
-	double constA;
-	double constB;
-	double area;
-}TBRTDproperties;
+#include "i-v_char.h"
+
+#endif
+
 
 
 // electron suppling
@@ -115,33 +94,3 @@ double j_tbrtd(double v, TBRTDproperties prp)
 	double j_th = j_thermal(v, prp);
 	return j_th * prp.constB + (j_rtd_l + j_rtd_r) * prp.constA;
 }
-
-/*int main(void)
-{
-	TBRTDproperties TBRTD;
-	TBRTD.V_e = 0.36;
-	TBRTD.J_e = 0.60e-3;
-	TBRTD.T = 100;
-	TBRTD.n = 3.7;
-	TBRTD.sigma = 20.0e-3;
-	TBRTD.gamma_l = 16.0e-3;
-	TBRTD.xi = calc_xi(TBRTD.sigma, TBRTD.gamma_l);
-	TBRTD.E_F = 40e-3;
-	TBRTD.e = 1.602e-19;
-	TBRTD.k_B = 1.38e-23/e;
-	TBRTD.hbar = 1.05e-34;
-	TBRTD.m0 = 9.11e-31;
-	TBRTD.m = TBRTD.m0 * 0.0041;
-	TBRTD.E_l0 = 44.7e-3;
-	TBRTD.E_r0 = 97.2e-3;
-	TBRTD.Eta_l = 0.353;
-	TBRTD.Eta_r = 0.71;
-	TBRTD.constA = 3.0e-28;
-	TBRTD.constB = 3.0e6;
-
-	double i;
-	for(i=0; i < 0.4; i+=0.0002) {
-		fprintf(stdout, "%E %E\n", i, j_tbrtd(i, TBRTD));
-	}
-	return EXIT_SUCCESS;
-}*/
